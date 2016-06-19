74cab2dd56bdbe486f1d82401144aa1c2ebdb6ec
Amandine Perichon <apericho@gmail.com>
2016-06-20 11:22:11 +1200

    Add object board and get row number

--- minesweeper.js
+++ minesweeper.js
@@ -4,6 +4,10 @@ function startGame () {
   addListeners(document.getElementsByClassName('board')[0].children)
 }
 
+var board = {
+  cell: []
+}
+
 // Add event listener click on all elements of the board
 function addListeners (elements) {
   for (var i = 0; i < elements.length; i++) {
@@ -21,5 +25,15 @@ function showCell (evt) {
 function markCell (evt) {
   evt.preventDefault()
   evt.target.classList.toggle('marked')
+}
+
+// Get row of an element
+function getRow (element) {
+  var classes = element.classList
+  for (var i = 0; i < classes.length; i++) {
+    if (classes[i].substring(0,3) === 'row') {
+      return classes[i].split('-')[1]
+    }
+  }
 }
 
