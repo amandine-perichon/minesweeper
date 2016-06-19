150367313981411198848535d72608c7f0b11fd5
Amandine Perichon <apericho@gmail.com>
2016-06-20 10:47:27 +1200

    Show content of a cell

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  addListeners(document.getElementsByClassName('board')[0].children)
 }
+
+// Add event listener click on all elements of the board
+function addListeners (elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', showCell)
+  }
+}
+
+// Show content of a cell: white or bomb
+function showCell (evt) {
+  evt.target.classList.remove('hidden')
+}
+
