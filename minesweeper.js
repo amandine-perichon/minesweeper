ea0040f4ee99eca4525ededc9347699418a2ff52
Amandine Perichon <apericho@gmail.com>
2016-06-20 11:40:31 +1200

    Populate board object with cell information

--- minesweeper.js
+++ minesweeper.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame () {
-  addListeners(document.getElementsByClassName('board')[0].children)
-}
-
 var board = {
   cell: []
 }
 
-// Add event listener click on all elements of the board
-function addListeners (elements) {
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', showCell)
-    elements[i].addEventListener('contextmenu', markCell)
+function startGame () {
+  var boardCells = document.getElementsByClassName('board')[0].children
+  for (var i = 0; i < boardCells.length; i++) {
+    addListeners(boardCells[i])
+    addCellToBoard(boardCells[i])
   }
+}
+
+// Add event listener click on all elements of the board
+function addListeners (element) {
+  element.addEventListener('click', showCell)
+  element.addEventListener('contextmenu', markCell)
+}
+
+// Add cell data to board object
+function addCellToBoard (element) {
+  var newCell = {}
+  newCell.row = getRow(element)
+  newCell.col = getCol(element)
+  newCell.isMine = element.classList.contains('mine')
+  board.cell.push(newCell)
 }
 
 // Show content of a cell: white or bomb
@@ -27,21 +38,21 @@ function markCell (evt) {
   evt.target.classList.toggle('marked')
 }
 
-// Get row of an element
+// Get row number of an element
 function getRow (element) {
   var classes = element.classList
   for (var i = 0; i < classes.length; i++) {
-    if (classes[i].substring(0,3) === 'row') {
+    if (classes[i].substring(0, 3) === 'row') {
       return classes[i].split('-')[1]
     }
   }
 }
 
-// Get row of an element
+// Get column number of an element
 function getCol (element) {
   var classes = element.classList
   for (var i = 0; i < classes.length; i++) {
-    if (classes[i].substring(0,3) === 'col') {
+    if (classes[i].substring(0, 3) === 'col') {
       return classes[i].split('-')[1]
     }
   }
